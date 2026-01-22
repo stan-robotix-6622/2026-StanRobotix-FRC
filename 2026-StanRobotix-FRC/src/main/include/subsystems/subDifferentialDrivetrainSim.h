348aa7f22f009c457e3440d157c2a33c8ac92f13
Stan Robotix <stanrobotix@stanislas.qc.ca>
2026-01-21 19:37:07 -0500

    Creation of Encoder and subDifferentialDrive... step 3 of the tuto

---
+++ 2026-StanRobotix-FRC/src/main/include/subsystems/subDifferentialDrivetrainSim.h
@@ -0,0 +1,59 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc2/command/SubsystemBase.h>
+#include <frc/Encoder.h>
+#include <frc/simulation/EncoderSim.h>
+#include <frc/simulation/DifferentialDrivetrainSim.h>
+
+
+
+class subDifferentialDrivetrainSim : public frc2::SubsystemBase {
+ public:
+  subDifferentialDrivetrainSim();
+
+  /**
+   * Will be called periodically whenever the CommandScheduler runs.
+   */
+  void Periodic() override;
+
+ private:
+  // Components (e.g. motor controllers and sensors) should generally be
+  // declared private and exposed only through public methods.
+
+
+  // These represent our regular encoder objects, which we would
+  // create to use on a real robot.
+  frc::Encoder m_leftEncoder{0, 1};
+  frc::Encoder m_rightEncoder{2, 3};
+  // These are our EncoderSim objects, which we will only use in
+  // simulation. However, you do not need to comment out these
+  // declarations when you are deploying code to the roboRIO.
+  frc::sim::EncoderSim m_leftEncoderSim{m_leftEncoder};
+  frc::sim::EncoderSim m_rightEncoderSim{m_rightEncoder};
+  
+// Create the simulation model of our drivetrain.
+frc::sim::DifferentialDrivetrainSim m_driveSim{
+  frc::DCMotor::CIM(2), // 2 CIM motors on each side of the drivetrain.
+  7.29,               // 7.29:1 gearing reduction.
+  7.5_kg_sq_m,        // MOI of 7.5 kg m^2 (from CAD model).
+  60_kg,              // The mass of the robot is 60 kg.
+  3_in,               // The robot uses 3" radius wheels.
+  0.7112_m,           // The track width is 0.7112 meters.
+  // The standard deviations for measurement noise:
+  // x and y:          0.001 m
+  // heading:          0.001 rad
+  // l and r velocity: 0.1   m/s
+  // l and r position: 0.005 m
+  {0.001, 0.001, 0.001, 0.1, 0.1, 0.005, 0.005}};
+
+};
+frc::sim::DifferentialDrivetrainSim m_driveSim =
+  frc::sim::DifferentialDrivetrainSim::CreateKitbotSim(
+    frc::sim::DifferentialDrivetrainSim::KitbotMotor::DualCIMPerSide, // 2 CIMs per side.
+    frc::sim::DifferentialDrivetrainSim::KitbotGearing::k10p71,       // 10.71:1
+    frc::sim::DifferentialDrivetrainSim::KitbotWheelSize::kSixInch    // 6" diameter wheels.
+);
