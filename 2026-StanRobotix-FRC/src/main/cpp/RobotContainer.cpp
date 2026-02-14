e16f01b0d3c32ace9784013aeed8af3ad79be30c
Stan Robotix <stanrobotix@stanislas.qc.ca>
2026-02-13 21:37:44 -0500

    Change Intake max speed

--- 2026-StanRobotix-FRC/src/main/cpp/RobotContainer.cpp
+++ 2026-StanRobotix-FRC/src/main/cpp/RobotContainer.cpp
@@ -34,7 +34,7 @@ void RobotContainer::ConfigureBindings() {
 
   m_driverController->Y().WhileTrue(PivotIntake(m_SubPivotIntake, PivotIntake::StatePivotIntake::kUp).ToPtr());
   m_driverController->B().WhileTrue(PivotIntake(m_SubPivotIntake, PivotIntake::StatePivotIntake::kDown).ToPtr());
-  m_driverController->X().WhileTrue(frc2::cmd::Run([this] {m_SubIntake->SetVoltage(m_driverController->GetRightTriggerAxis() * 1.0);}, {m_SubIntake}));
+  m_driverController->X().WhileTrue(frc2::cmd::Run([this] {m_SubIntake->SetVoltage(m_driverController->GetRightTriggerAxis() * 3.0);}, {m_SubIntake}));
   // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
   frc2::Trigger([this] {
     return m_subsystem.ExampleCondition();
