e3a6297ce829c310d8d2ad9117fcf1780fc7ffa3
Starelie <timotheelaberge@gmail.com>
2026-05-02 00:58:17 -0400

    Move getMatchStatus to the rebuilt folder

---
+++ src/rebuilt/include/rebuilt/MatchStatus.h
@@ -0,0 +1,30 @@
+#include <string_view>
+
+#include <units/time.h>
+
+namespace Rebuilt
+{
+  namespace Match
+  {
+    enum Period {
+      Autonomous,
+      TransitionShift,
+      Shift1,
+      Shift2,
+      Shift3,
+      Shift4,
+      Endgame
+    };
+
+    struct Status
+    {
+      bool hubActive;
+      Period matchPeriod;
+      std::string_view matchPeriodName;
+      units::second_t timeLeftInPeriod;
+      units::second_t timeLeftInMatch;
+    };
+
+  }
+  Match::Status getMatchStatus();
+}	 // namespace Rebuilt
